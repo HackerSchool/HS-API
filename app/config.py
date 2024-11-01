a03c361f5bf46fc3c1261228ce1af6a6977d1184
gazev <goncalo.r.azevedo@tecnico.ulisboa.pt>
2024-11-01 17:37:15 +0000

    fix: change default env values

--- app/config.py
+++ app/config.py
@@ -18,9 +18,9 @@ def _get_env_or_default(env: str, default: str, cast=None):
 class Config:
     SECRET_KEY = _get_env_or_default("SECRET_KEY", secrets.token_hex(256))
     SESSION_TYPE = _get_env_or_default("SESSION_TYPE", 'filesystem')  # Store session data in the filesystem
-    SESSION_FILE_DIR = _get_env_or_default("SESSION_DIR", os.path.join(os.getcwd(), 'flask_session'))  # Directory to store session files
-    SESSION_PERMANENT = _get_env_or_default("SESSION_PERMANENT", True, lambda x : x.lower == "true")# Whether to use permanent sessions
-    SESSION_USE_SIGNER = _get_env_or_default("SESSION_USE_SIGNER", True, lambda x : x.lower == "true")  # Whether to sign the session ID cookie for security
+    SESSION_FILE_DIR = _get_env_or_default("SESSION_DIR", os.path.join(os.getcwd(), 'data/flask_session'))  # Directory to store session files
+    SESSION_PERMANENT = _get_env_or_default("SESSION_PERMANENT", True, lambda x : x.lower == "true")        # Whether to use permanent sessions
+    SESSION_USE_SIGNER = _get_env_or_default("SESSION_USE_SIGNER", True, lambda x : x.lower == "true")      # Whether to sign the session ID cookie for security
     SESSION_KEY_PREFIX = _get_env_or_default("SESSION_KEY_PREFIX", 'my_session_')  # Prefix for session files
     session_expiration = 30
     try:
@@ -29,6 +29,6 @@ class Config:
         pass
     PERMANENT_SESSION_LIFETIME = session_expiration 
  
-    DATABASE_PATH = _get_env_or_default("DATABASE_PATH", 'hackerschool.db')
-    TAGS_PATH = _get_env_or_default("TAGS_PATH", 'tags.json')
-    LOGOS_PATH = _get_env_or_default("LOGOS_PATH", 'logos/')
+    DATABASE_PATH = _get_env_or_default("DATABASE_PATH", 'data/hackerschool.sqlite3')
+    TAGS_PATH = _get_env_or_default("TAGS_PATH", 'data/tags.json')
+    LOGOS_PATH = _get_env_or_default("LOGOS_PATH", 'data/logos/')
