22fcd7a2c5f6bcdecf4a62482a187ae745195034
gazev <goncalo.r.azevedo@tecnico.ulisboa.pt>
2024-11-13 20:19:47 +0000

    fix: disable SESSION_REFRESH_EACH_REQUEST, remove default log file

--- app/__init__.py
+++ app/__init__.py
@@ -58,20 +58,18 @@ def register_error_handlers(app: Flask):
     app.register_error_handler(exc.SQLAlchemyError, handle_db_exceptions)
 
 def setup_logger(app: Flask):
-    if app.debug: # don't set logger in debug
+    if app.debug or app.config.get("LOGS_PATH", "") == "": # don't set logger in debug
         return
 
     levels = {"DEBUG": logging.DEBUG, "INFO": logging.INFO, "WARNING": logging.WARNING}
-    app.logger.setLevel(levels[app.config["LOG_LEVEL"]])
+    app.logger.setLevel(levels[app.config.get("LOG_LEVEL")])
 
-    logs_path = app.config["LOGS_PATH"]
-    if logs_path is not None:
-        log_dir = os.path.dirname(logs_path)
-        if not os.path.exists(log_dir):
-            os.makedirs(log_dir)
-        handler = logging.FileHandler(logs_path)
-        BASIC_FORMAT = "[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s"  
-        handler.setFormatter(logging.Formatter(BASIC_FORMAT))
-        app.logger.addHandler(handler)
-
-        logging.getLogger("werkzeug").addHandler(handler)  # Root logger for all logs
+    logs_path = app.config.get("LOGS_PATH")
+    log_dir = os.path.dirname(logs_path)
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+    handler = logging.FileHandler(logs_path)
+    BASIC_FORMAT = "[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s"  
+    handler.setFormatter(logging.Formatter(BASIC_FORMAT))
+    app.logger.addHandler(handler)
+    logging.getLogger("werkzeug").addHandler(handler)  # Root logger for all logs
--- app/config.py
+++ app/config.py
@@ -24,19 +24,20 @@ def _get_int_env_or_default(env: str, default: int) -> int:
 class Config:
     SECRET_KEY = _get_env_or_default("SECRET_KEY", secrets.token_hex(256))
 
-    SESSION_TYPE               = "filesystem"
-    PERMANENT_SESSION          = True 
-    PERMANENT_SESSION_LIFETIME = _get_int_env_or_default("SESSION_LIFETIME", 12 * 60 * 60)
-    SESSION_KEY_PREFIX         = _get_env_or_default("SESSION_KEY_PREFIX", "hs_session_")
-    SESSION_FILE_DIR           = os.path.join(basedir, _get_env_or_default("SESSION_DIR", "data/flask_sessions")).rstrip("/")
+    SESSION_TYPE                 = "filesystem"
+    PERMANENT_SESSION            = True 
+    SESSION_REFRESH_EACH_REQUEST = False
+    PERMANENT_SESSION_LIFETIME   = _get_int_env_or_default("SESSION_LIFETIME", 12 * 60 * 60)
+    SESSION_KEY_PREFIX           = _get_env_or_default("SESSION_KEY_PREFIX", "hs_session_")
+    SESSION_FILE_DIR             = os.path.join(basedir, _get_env_or_default("SESSION_DIR", "data/flask_sessions/")).rstrip("/")
 
-    ROLES_PATH = os.path.join(basedir, _get_env_or_default("ROLES_PATH", "data/roles.json")).rstrip("/")
+    ROLES_PATH = os.path.join(basedir, _get_env_or_default("ROLES_PATH", "data/roles.json"))
 
-    DATABASE_PATH           = os.path.join(basedir, _get_env_or_default("DATABASE_PATH", "data/hackerschool.sqlite3")).rstrip("/")
+    DATABASE_PATH           = os.path.join(basedir, _get_env_or_default("DATABASE_PATH", "data/hackerschool.sqlite3"))
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
 
     PHOTOS_DIR         = os.path.join(basedir, _get_env_or_default("PHOTOS_DIR", "data/photos/")).rstrip("/")
     MAX_CONTENT_LENGTH = _get_int_env_or_default("MAX_PHOTO_SIZE", 16 * 1000 * 1000) ## max pohto size
 
-    LOGS_PATH = os.path.join(basedir, _get_env_or_default("LOGS_PATH", "data/logs/app.log")).rstrip("/")
+    LOGS_PATH = os.path.join(basedir, _get_env_or_default("LOGS_PATH", ""))
     LOG_LEVEL = _get_env_or_default("LOG_LEVEL", "INFO")
