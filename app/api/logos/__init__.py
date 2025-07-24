bfc00e5dab5576e053a9b5dae161dc7df16e991b
goncalorazevedo <goncalo.r.azevedo@tecnico.ulisboa.pt>
2025-07-24 08:19:49 +0100

    refactor: delete src

--- app/api/logos/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('logos', __name__)
-
-from app.api.logos import routes
--- app/api/logos/utils.py
+++
@@ -1,4 +0,0 @@
-def is_a_link(link):
-    if link.startswith("http://") or link.startswith("https://"):
-        return True
-    return False
